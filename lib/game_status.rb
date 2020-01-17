ba642703f9cb5c11d4da4d110a67fb65b18d314c
Jenise Ruehle <jela07@gmail.com>
2020-01-17 19:53:16 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,6 +20,9 @@ def won?(board)
     if (board[win_combo[0]]) == "X" && (board[win_combo[1]]) == "X" && (board[win_combo[2]]) == "X"
       return win_combo
     elsif (board[win_combo[0]]) == "O" && (board[win_combo[1]]) == "O" && (board[win_combo[2]]) == "O"
+      return win_combo
+    else
+      return false
   end
 end   
   
